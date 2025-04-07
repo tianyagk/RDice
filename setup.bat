d1494eafae903d90e0f5fe3fdad0410356d38798
tianyagk <270352204@qq.com>
2025-04-07 22:06:36 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,13 +1,31 @@
 @echo off
 :: 定义虚拟环境激活脚本路径
 set "VENV_ACTIVATE=.venv\Scripts\activate"
+
+:: 如果虚拟环境不存在，则创建并安装依赖
 if not exist "%VENV_ACTIVATE%" (
+    echo Creating Env...
     python -m venv .venv --prompt nonebot2
-    python.exe -m pip install --upgrade pip
+    if errorlevel 1 (
+        echo Env created fail!
+        pause
+        exit /b 1
+    )
+    echo Activating Env and install requirements...
+    call ".venv\Scripts\activate"
+    python -m pip install --upgrade pip
     pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+    if errorlevel 1 (
+        echo requirements install fail！
+        pause
+        exit /b 1
+    )
+) else (
+    call ".venv\Scripts\activate"
 )
 
-.venv\Scripts\activate
-
-# 启动Bot脚本
+:: 启动 Bot
+echo Launching NoneBot2 ...
 python bot.py
+
+pause
